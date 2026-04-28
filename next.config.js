1ab66ef73cd186493d05fd58fe84524b397ee544
rumaan121 <rumaansadiq@gmail.com>
2026-04-29 04:29:43 +0500

    Fix Tailwind and TypeScript config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export'
+  output: 'export',   // 👈 this replaces "next export"
 };
 
 module.exports = nextConfig;
