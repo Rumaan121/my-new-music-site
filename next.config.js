bce5df317e73142931520c09f869462d8c76af5d
rumaan121 <rumaansadiq@gmail.com>
2026-05-05 18:08:41 +0500

    Update next.config.js and docs trace

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-//** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',   // enables static HTML export
-  distDir: 'docs'     // puts the build directly into /docs for GitHub Pages
+  output: 'export',        // enables static HTML export
+  distDir: 'docs',         // put the build directly into /docs for GitHub Pages
+  images: { unoptimized: true } // disables Next.js image optimization (not supported on Pages)
 };
 
 module.exports = nextConfig;
-
